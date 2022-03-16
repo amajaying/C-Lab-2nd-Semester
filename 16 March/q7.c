34310a9817035f4cfaa3698fde60bc30f85fc04d
Ajay Khatri <amajaying@gmail.com>
2022-03-16 21:27:57 +0530

    16 march class

--- 16 March/q7.c
+++ 16 March/q7.c
@@ -3,7 +3,7 @@
 int main()
 {
     char c = '\0';
-    printf("Item No%5cName%10cPrice", c, c);
+    printf("Item No%6cName%10cPrice", c, c);
     printf("\n10%12cxyp%10c$1929.63", c, c);
     printf("\n120%10cmptm%10c$26.30", c, c);
     printf("\n8%8ckjhssaas%10c$1929.63", c, c);
