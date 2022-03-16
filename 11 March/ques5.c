90430d0efab4fb32e2bd6410100d1c7ef8ccbe6e
Ajay Khatri <amajaying@gmail.com>
2022-03-16 08:20:53 +0530

    16 march class

--- 11 March/ques5.c
+++ 11 March/ques5.c
@@ -1,1 +1,13 @@
-//
+// take 3 nos
+
+#include <stdio.h>
+int main()
+{
+    int x, y, z;
+    scanf("%d%d%d", &x, &y, &z);
+    x = x + y + z;
+    y = x - y - z;
+    z = x - y - z;
+    x = x - y - z;
+    printf("After swapping:%d %d %d", x, y, z);
+}
