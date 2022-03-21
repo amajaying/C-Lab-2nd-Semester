9f0fe63a5b7955d1b69eae2fefbe29b98a7433ab
Ajay Khatri <amajaying@gmail.com>
2022-03-21 15:39:13 +0530

    done

--- 18 March/ques10.c
+++ 18 March/ques10.c
@@ -1,30 +1,20 @@
 // take two timie in hrs, min and find difference
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     // declaring and taking inputs
-    int hr1, hr2, min1, min2, h;
+    int hr1, hr2, min1, min2, hr, min;
     printf("Enter first time in hours and minutes: ");
     scanf("%d%d", &hr1, &min1);
     printf("Enter second time in hours and minutes: ");
     scanf("%d%d", &hr2, &min2);
 
-    // converting hours into minutes
-    int t1 = hr1 * 60 + min1;
-    int t2 = hr2 * 60 + min2;
+    min = (hr1 > hr2) ? ((min1 > min2) ? (min1 - min2) : (60 + min1 - min2)) : 0;
+    hr = hr1 - hr2;
 
-    // calculation
-    int s = (t2 > t1) ? 0 : 1;
-    float diff = t1 - t2;
-    float a = diff / 60;
-    int hr = a;
-
-    // minutes into hours
-    int b = (a - hr) * 60;
-
-    // checking & printing
-    (s == 1) ? printf("The time difference: %i hours and %i minutes", hr, b) : printf("Can't be determined!");
+    (min != 0) ? printf("The time difference: %i hours and %i minutes", hr, min) : printf("Can't be determined!");
 
     return 0;
 }
