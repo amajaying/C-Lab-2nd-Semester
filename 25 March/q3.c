d7e094e21f73538de4e1fdb65e8a7a93c131b8bf
Ajay Khatri Chhetri <amajaying@gmail.com>
2022-03-25 11:05:36 +0530

    changes

--- 25 March/q3.c
+++ 25 March/q3.c
@@ -2,12 +2,12 @@
 #include<stdio.h>
 int main(){
 	int h1,m1,h2,m2,h=0,m=0;
-	printf("Enter the 1st time: ");
-	scanf("%d:%d",&h1,&m1);
-	printf("Enter the 2nd time: ");
-	scanf("%d:%d",&h2,&m2);
+	printf("Enter the 1st time (HH:MM) with (AM =1 and PM =0): ");
+	scanf("%d:%d%d",&h1,&m1);
+	printf("Enter the 2nd time (HH:MM) with (AM =1 and PM =0): ");
+	scanf("%d:%d%d",&h2,&m2);
 	
-	if (h2>h1||(h2>h1&&m2>m1)){
+	if (h2>h1||(h2>h1&&m2>m1){
 		printf("%d:%d is earlier",h1,m1);
 		
 		if(m1<m2){
