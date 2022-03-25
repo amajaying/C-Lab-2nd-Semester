828eb33ae27caff260bdc6240efb5fc6b1464f19
Ajay Khatri <amajaying@gmail.com>
2022-03-25 08:55:02 +0530

    updates

--- 23 March/q5.c
+++ 23 March/q5.c
@@ -4,8 +4,8 @@ int main()
 {
     int a = 0x5A34BDA2;
 
-    int b = (a & 0x00FF0000) >> 8;
-    int c = (a & 0x0000FF00) << 8;
+    int b = (a & 0xFF0000) >> 8;
+    int c = (a & 0xFF00) << 8;
     int d = a & 0xFF0000FF;
 
     int r = b | c | d;
--- 23 March/q7.c
+++ 23 March/q7.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 int main()
 {
-    int a;
-    scanf("%d", &a);
-    int b1 = (a & 0x000F);
-    int b2 = (a & 0x00F0) >> 4;
-    int b3 = (a & 0x0F00) >> 8;
-    int b4 = (a & 0xF000) >> 12;
-    printf("1st byte: %i \n2nd byte: %i\n3rd byte: %i\n4th byte: %i", b1, b2, b3, b4);
+    int a = 0x465dfea4;
+    int b1 = (a & 0xFF);
+    int b2 = (a & 0xFF00) >> 8;
+    int b3 = (a & 0xFF0000) >> 16;
+    int b4 = (a & 0xFF000000) >> 24;
+    printf("1st byte: %x \n2nd byte: %x\n3rd byte: %x\n4th byte: %x", b1, b2, b3, b4);
 }
