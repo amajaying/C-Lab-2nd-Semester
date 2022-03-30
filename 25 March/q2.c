26a1955ee405e1adb8cfbbe10a938ca3b115d7e3
Ajay Khatri <amajaying@gmail.com>
2022-03-30 08:50:25 +0530

    25 march

--- 25 March/q2.c
+++ 25 March/q2.c
@@ -1,16 +1,16 @@
-// take 2 times in __:__  and find diff
+// take 2 times in __:__  and find diff also am pm
 #include <stdio.h>
 int main()
 {
-	int h1, m1, h2, m2, h = 0, m = 0;
+	int h1, m1, h2, m2, h = 0, m = 0, a1, a2;
 	printf("Enter the 1st time (HH:MM) with (AM =1 and PM =0): ");
-	scanf("%d:%d", &h1, &m1);
+	scanf("%d:%d%d", &h1, &m1, &a1);
 	printf("Enter the 2nd time (HH:MM) with (AM =1 and PM =0): ");
-	scanf("%d:%d", &h2, &m2);
+	scanf("%d:%d%d", &h2, &m2, &a2);
 
 	if (h2 > h1 || (h2 > h1 && m2 > m1))
 	{
-		printf("%d:%d is earlier", h1, m1);
+		printf("%d:%d %d is earlier", h1, m1);
 
 		if (m1 < m2)
 		{
--- 25 March/q3.c
+++ 25 March/q3.c
@@ -1,1 +1,126 @@
+// find 2nd largest and 3rd largest number among 4 nos
+#include <stdio.h>
 
+int main()
+{
+    int a, b, c, d, t, s, max, r1, r2, result;
+    scanf("%d%d%d%d", &a, &b, &c, &d);
+
+    if (a > b)
+    {
+        s = a;
+    }
+    else
+    {
+        s = b;
+    }
+
+    if (c > d)
+    {
+        t = c;
+    }
+    else
+    {
+        t = d;
+    }
+
+    max = (s > t) ? s : t;
+
+    if (a > b)
+    {
+        if (a == max)
+        {
+            r1 = b;
+        }
+        else
+        {
+            r1 = a;
+        }
+    }
+    else
+    {
+        if (b == max)
+        {
+            r1 = a;
+        }
+        else
+        {
+            r1 = b;
+        }
+    }
+
+    if (c > d)
+    {
+        if (c == max)
+        {
+            r2 = d;
+        }
+        else
+        {
+            r2 = c;
+        }
+    }
+    else
+    {
+        if (d == max)
+        {
+            r2 = c;
+        }
+        else
+        {
+            r2 = d;
+        }
+    }
+
+    result = (r1 > r2) ? r1 : r2;
+    printf("2nd largest number = %d", result);
+
+    if (a > b)
+    {
+        if (a == max || a == result)
+        {
+            r1 = b;
+        }
+        else
+        {
+            r1 = a;
+        }
+    }
+    else
+    {
+        if (b == max || b == result)
+        {
+            r2 = a;
+        }
+        else
+        {
+            r2 = b;
+        }
+    }
+
+    if (c > d)
+    {
+        if (c == max)
+        {
+            r1 = d;
+        }
+        else
+        {
+            r1 = c;
+        }
+    }
+    else
+    {
+        if (d == max)
+        {
+            r2 = c;
+        }
+        else
+        {
+            r2 = d;
+        }
+    }
+
+    result = (r1 < r2) ? r1 : r2;
+    printf("\nThird largest = %d", result);
+}
