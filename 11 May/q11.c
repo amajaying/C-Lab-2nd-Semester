85b5a66d884e0ec832f4edbd48868e2c76393954
Ajay Khatri <amajaying@gmail.com>
2022-05-11 10:51:44 +0530

    changes

--- 11 May/q11.c
+++ 11 May/q11.c
@@ -10,11 +10,26 @@ int patt(int r, char t, char l){
             printf("\n");
         }
     }
+
+    if(l=='d'){
+        for(int i=0; i<=r; i++){
+            for (int j=0; j<i; j++){
+                printf("%c",t);
+            }  
+            printf("\n");
+        }
+    }
 }
+
 
 int main(){
     int r,c;
-    patt(4,'*','u');
-
+    patt(4,'*','d');
+    printf("ABC\n");
+    patt(3,'#','u');
+    printf("DEF");
+    patt(3,'=','d');
+    printf("pqr\n");
+    patt(2,'*','u');
 
 }
