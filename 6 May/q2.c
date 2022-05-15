37232584c65ad50524660730d39c448d0dc669a9
amajaying <amajaying@gmail.com>
2022-05-15 12:38:17 +0530

    changes

--- 6 May/q2.c
+++ 6 May/q2.c
@@ -4,8 +4,14 @@ int main(){
     char a[10];
     scanf("%[^\n]s",a);
     printf("Before reversing: %s", a);
-    char b[10];    
-    for(int i=0,j=7; j>=0; i++,j--){
+    char b[10];
+
+    int c=0;
+    while(a[c]!='\0'){
+        c++;
+    }
+      
+    for(int i=0,j=c-1; j>=0; i++,j--){
         b[i]=a[j];
     }
     printf("\nAfter reversing: %s", b);
