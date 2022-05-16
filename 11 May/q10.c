be61741a13f5e8a1a1391e4522eb6fc1a1fda047
amajaying <amajaying@gmail.com>
2022-05-16 21:38:57 +0530

    changes

--- 11 May/q10.c
+++ 11 May/q10.c
@@ -1,5 +1,29 @@
 // 1/1! + 1/3! + 2/5! + 3/7! + 5/9! + 8/11!
 #include<stdio.h>
-int main(){
-    
+int fib(int n)
+{
+   if (n <= 1)
+      return n;
+   return fib(n-1) + fib(n-2);
+}
+
+int fact(int n)  
+{  
+  if (n == 0)  
+    return 1;  
+  else  
+    return(n * fact(n-1));  
+}  
+
+int main ()
+{
+  int n = 1;
+  int sum = 0;
+  int a = n;
+  for (int i = 0; i <n; i++){
+    sum = sum + (fib(n)/fact(a));
+    a+=2;
+  }
+  printf("%d", sum);
+  return 0;
 }
--- 11 May/q7.c
+++ 11 May/q7.c
@@ -4,7 +4,7 @@
 int fact(int n){
     int fact=1;
     while (n>0)
-    {
+    { 
         fact=fact*n;
         n--;
     }
