c1dc95830d5c825741fc8efaea0a31b32389ad03
amajaying <amajaying@gmail.com>
2022-05-16 22:02:27 +0530

    changes

--- 11 May/q6.c
+++ 11 May/q6.c
@@ -5,7 +5,7 @@
 #include<stdio.h>
 int main(){
     char a[50];
-    int spc;
+    int spc=0;
     scanf("%[^\n]s",a);
 
     int i,j,k=0;
@@ -17,15 +17,29 @@ int main(){
     for(int i=0; i<spc; i++){
         printf("%c",a[i]);
     }
-
-    while (a[spc]!='\0'){
+    printf(" ");
+    int r =0;
+    int count = 0;
+    for(int i=0; a[i]!='\0'; i++){
+        count++;
+        
+    }
+    for(int i=count-1; a[i]!=' '; i--){
+        r++;
+ 
+    }
+    for(i=spc;i<r;i++)
     {
-       if(a[i]==' ')
-       {
-         a[i+1]=a[i+1]+32;
-         printf("%c. ",a[i+1]);
-       }
+        if(a[i]==' ')
+        {
+            printf("%c. ",a[i+1]);
+        }
     }
+
+       
+    for (int j = count-r; j < count; j++){
+        printf("%c", a[j]);
     }
+    
 }
 
