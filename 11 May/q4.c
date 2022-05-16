0e33d9dc41e2ff161c0cc25a637dc3af7e517671
amajaying <amajaying@gmail.com>
2022-05-16 19:55:24 +0530

    changed

---
+++ 11 May/q4.c
@@ -0,0 +1,26 @@
+// Take a string and reverse it
+#include<stdio.h>
+int main(){
+    char a[100];
+    int i,k=0;
+    printf("Enter a string\n");
+    scanf("%[^\n]s",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]==' ')
+        {
+            for(int j=i-1;j>=k;j--)
+            {
+                printf("%c",a[j]);
+            }
+            k=i+1;
+            printf(" ");
+        }
+            
+    }
+    for(int j=i;j>=k;j--)
+            {
+                printf("%c",a[j]);
+            }
+
+}
