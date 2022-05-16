d265e9121a7ed8ec393fc9a2c40c8d0698674c5c
amajaying <amajaying@gmail.com>
2022-05-16 21:25:09 +0530

    changes

--- 11 May/q5.c
+++ 11 May/q5.c
@@ -7,12 +7,14 @@ int main(){
     char a[50];
     scanf("%[^\n]s",a);
     int i,j,k=0;
-    a[0]=a[0]+32;
     for(i=1;a[i]!='\0';i++)
     {
         if(a[i]==' ')
         {
-            a[i+1]=a[i+1]+32;
+            i++;
+        }
+        else{
+            a[i]=a[i]+32;
         }
     }
 printf("%s",a);
--- 11 May/q6.c
+++ 11 May/q6.c
@@ -1,20 +1,31 @@
 //Take a string and display in the following format 
-
 // AbcD PklM nT  VPR
-//A. P. N. Vpr]
+//A. P. N. Vpr
 
 #include<stdio.h>
 int main(){
     char a[50];
+    int spc;
     scanf("%[^\n]s",a);
+
     int i,j,k=0;
-    a[0]=a[0]-32;
-    printf("%c.",a[0]);
-    for(i=1;a[i]!='\0';i++)
+
+    while(a[spc]!=' '){
+        spc++;
+    }
+
+    for(int i=0; i<spc; i++){
+        printf("%c",a[i]);
+    }
+
+    while (a[spc]!='\0'){
     {
-        if(a[i]==' ')
-        {
-            printf(" %c.",a[i+1]-32);
-        }
+       if(a[i]==' ')
+       {
+         a[i+1]=a[i+1]+32;
+         printf("%c. ",a[i+1]);
+       }
+    }
     }
 }
+
