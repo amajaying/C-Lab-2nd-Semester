59f18a0b58099ee6e5a4d9efb7ef36d9cc0a97c0
amajaying <amajaying@gmail.com>
2022-05-16 15:07:17 +0530

    changes

--- 6 May/q7.c
+++ 6 May/q7.c
@@ -1,13 +1,19 @@
-#include <stdio.h>
-int main()
-{
-    int ar[3][3];
+// Find the determinant of 3*3
+#include<stdio.h>
+int main(){
 
+    int arr[3][3];
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-            
+            scanf("%d", &arr[i][j]);
         }
     }
-}
+    int det=0;
+    for(int i=0;i<3;i++){
+        det+= arr[0][i]*(arr[1][(i+1)%3]*arr[2][(i+2)%3]-arr[1][(i+2)%3]*arr[2][(i+1)%3]);
+    }
+    printf("%d",det);
+    return 0;
+}                                 
