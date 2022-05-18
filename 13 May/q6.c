7c93ef7a0ceef2fca467055f636df54a8e85c2c7
amajaying <amajaying@gmail.com>
2022-05-18 20:56:48 +0530

    changes

--- 13 May/q6.c
+++ 13 May/q6.c
@@ -1,5 +1,15 @@
 // 6. WAF which will take a string and return back the number of characters present in the string.
 #include<stdio.h>
+int numb(char a[],int *count){
+    for (int i=0; a[i]!='\0'; i++){
+        *count = *count+1;
+    }
+}
 int main(){
+    char c[30];
+    int count=0;
+    scanf("%[^\n]s",c);
+    numb(c,&count);
+    printf("%d",count);
     
 }
