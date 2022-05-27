aba3e9f618750428eb95306ff7d9dee5cd8d918c
amajaying <amajaying@gmail.com>
2022-05-27 09:35:20 +0530

    changes

--- 25 May/q1.c
+++ 25 May/q1.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-int main(){
     struct stu
     {
         int roll;
@@ -12,31 +11,39 @@ int main(){
         }m;
     }s[3];
     
+    int display(struct stu a[3]){
+        printf("%d\t%s\t%f", a[0].roll,a[0].name,a[0].m.cgpa);
+    }
     int main(){
+        struct stu c;
         for(int i = 0; i<3; i++){
             printf("Enter the roll number: ");
             scanf("%d", &s[i].roll);
             printf("Enter the name of student: ");
             scanf("%s", s[i].name);
             printf("What are you providing? Marks [0], CGPA[1]: ");
-            scanf("%d", s[i].flag);
+            scanf("%d", &s[i].flag);
             if(s[i].flag==0){
                 printf("Enter the 3 marks: ");
                 for(int j=0;j<3;j++){
-                    scanf("%d",s[i].m.marks[j]);
+                    scanf("%d",&s[i].m.marks[j]);
                 }
             }
-            else if (flag==1)
+            else if (s[i].flag==1)
             {
                 printf("Enter the CGPA: ");
-                scanf("%f",s[i].m.cgpa);
+                scanf("%f",&s[i].m.cgpa);
             }
             
         }
+        int sum = 0;
         for(int k = 0; k<3; k++){
-            if(s[k].flag==0){
-                
+            for(int l = 0; l<3; l++){
+                if(s[k].flag==0){
+                    sum = sum + s[k].m.marks[l];
+                }
             }
+            s[k].m.cgpa = (sum/3)*10;
         }
+        display(c);
     }
-}
---
+++ 27 May/readme.md
@@ -0,0 +1,18 @@
+## Scope of the variable
+ Block
+ Program
+ Global
+ Function
+ File
+
+any variable with ``'static'`` cannot be used outside of the file in filescope
+
+## Storage Class:
+
+**auto specifier**
+
+**static specifier**
+** when you declare a variable with static it automatically starts with 0.
+** when you declare a variable with static it is not available outside of the file.
+** static int cant be declared twice in a same program.
+
