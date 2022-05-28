eb54fd9a3fe844f65d9c89aab62be0d87bef84be
amajaying <amajaying@gmail.com>
2022-05-28 22:03:24 +0530

    codes

--- 25 May/q1.c
+++ 25 May/q1.c
@@ -11,39 +11,91 @@
         }m;
     }s[3];
     
-    int display(struct stu a[3]){
-        printf("%d\t%s\t%f", a[0].roll,a[0].name,a[0].m.cgpa);
+    void display(struct stu a[3]){
+        printf("The Ascending Order:\n");
+        if (a[0].m.cgpa <= a[1].m.cgpa && a[1].m.cgpa <= a[2].m.cgpa){
+            printf("%d %s %.2f\n",a[0].roll,a[0].name,a[0].m.cgpa);
+            printf("%d %s %.2f\n",a[1].roll,a[1].name,a[1].m.cgpa);
+            printf("%d %s %.2f\n",a[2].roll,a[2].name,a[2].m.cgpa);
+  }
+  else{
+
+    if (a[0].m.cgpa <= a[2].m.cgpa && a[2].m.cgpa <= a[1].m.cgpa){
+            printf("%d %s %.2f\n",a[0].roll,a[0].name,a[0].m.cgpa);
+            printf("%d %s %.2f\n",a[2].roll,a[2].name,a[2].m.cgpa);
+            printf("%d %s %.2f\n",a[1].roll,a[1].name,a[1].m.cgpa);
+    }
+    else{
+
+      if (a[1].m.cgpa <= a[0].m.cgpa && a[0].m.cgpa <= a[2].m.cgpa){
+            printf("%d %s %.2f\n",a[1].roll,a[1].name,a[1].m.cgpa);
+           printf("%d %s %.2f\n",a[0].roll,a[0].name,a[0].m.cgpa);
+            printf("%d %s %.2f\n",a[2].roll,a[2].name,a[2].m.cgpa);
+      }
+      else{
+
+        if (a[1].m.cgpa <= a[2].m.cgpa && a[2].m.cgpa <= a[0].m.cgpa){
+            printf("%d %s %.2f\n",a[1].roll,a[1].name,a[1].m.cgpa);
+            printf("%d %s %.2f\n",a[2].roll,a[2].name,a[2].m.cgpa);
+           printf("%d %s %.2f\n",a[0].roll,a[0].name,a[0].m.cgpa);
+
+        }
+        else{
+
+          if (a[2].m.cgpa <= a[0].m.cgpa && a[0].m.cgpa <= a[1].m.cgpa){
+            printf("%d %s %.2f\n",a[2].roll,a[2].name,a[2].m.cgpa);
+           printf("%d %s %.2f\n",a[0].roll,a[0].name,a[0].m.cgpa);
+            printf("%d %s %.2f\n",a[1].roll,a[1].name,a[1].m.cgpa);
+
+          }
+          else{
+            if (a[0].m.cgpa == a[1].m.cgpa && a[1].m.cgpa == a[2].m.cgpa){
+              printf("%d %s %.2f\n",a[0].roll,a[0].name,a[0].m.cgpa);
+            printf("%d %s %.2f\n",a[1].roll,a[1].name,a[1].m.cgpa);
+            printf("%d %s %.2f\n",a[2].roll,a[2].name,a[2].m.cgpa);
+            }
+            else{            
+            printf("%d %s %.2f\n",a[2].roll,a[2].name,a[2].m.cgpa);
+            printf("%d %s %.2f\n",a[1].roll,a[1].name,a[1].m.cgpa);
+            printf("%d %s %.2f\n",a[0].roll,a[0].name,a[0].m.cgpa);
+            }
+          }
+        }
+      }
+    }
+}
     }
     int main(){
-        struct stu c;
+        struct stu c[3];
         for(int i = 0; i<3; i++){
             printf("Enter the roll number: ");
-            scanf("%d", &s[i].roll);
+            scanf("%d", &c[i].roll);
             printf("Enter the name of student: ");
-            scanf("%s", s[i].name);
+            scanf("%s", c[i].name);
             printf("What are you providing? Marks [0], CGPA[1]: ");
-            scanf("%d", &s[i].flag);
-            if(s[i].flag==0){
+            scanf("%d", &c[i].flag);
+            if(c[i].flag==0){
                 printf("Enter the 3 marks: ");
                 for(int j=0;j<3;j++){
-                    scanf("%d",&s[i].m.marks[j]);
+                    scanf("%d",&c[i].m.marks[j]);
                 }
             }
-            else if (s[i].flag==1)
+            else if (c[i].flag==1)
             {
                 printf("Enter the CGPA: ");
-                scanf("%f",&s[i].m.cgpa);
+                scanf("%f",&c[i].m.cgpa);
             }
             
         }
         int sum = 0;
-        for(int k = 0; k<3; k++){
-            for(int l = 0; l<3; l++){
-                if(s[k].flag==0){
-                    sum = sum + s[k].m.marks[l];
+            for(int k = 0; k<3; k++){
+                if(c[k].flag==0){
+                    for(int l = 0; l<3; l++){
+                            sum = sum + c[k].m.marks[l];
+                        }
+                c[k].m.cgpa = (sum/30)+1;
                 }
-            }
-            s[k].m.cgpa = (sum/3)*10;
         }
+        
         display(c);
     }
